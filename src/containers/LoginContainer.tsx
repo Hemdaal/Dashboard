27e323bcbea5f75fe57865c5160ffefb5f4e191a
Prasanna Anbazhagan <praslnx8@gmail.com>
2020-02-21 07:49:54 +0530

    Initialized with react and material ui.

---
+++ src/containers/LoginContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginComponent from "../components/LoginComponent";
+
+class LoginContainer extends React.Component{
+
+    render(): React.ReactNode {
+        return(<LoginComponent />)
+    }
+}
+
+export default LoginContainer
