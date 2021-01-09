be3d0887a49aa62c65db077025ae6d7edc6ef474
Prasanna Anbazhagan <praslnx8@gmail.com>
2021-01-09 23:59:09 +0530

    Added react grid layout

--- src/view/pages/projectDashboard/ProjectDashboardPageComponent.tsx
+++ src/view/pages/projectDashboard/ProjectDashboardPageComponent.tsx
@@ -5,6 +5,7 @@ import Container from "@material-ui/core/Container";
 import NavBarContainer from "../../shared/navbar/NavBarContainer";
 import LoadingComponent from "../../shared/LoadingComponent";
 import {ProjectDashboard} from "../../../models/ProjectDashboard";
+import GridLayout from 'react-grid-layout';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -46,9 +47,21 @@ export default function ProjectDashboardPageComponent(props: ProjectDashboardPag
 
 function getWidgetsComponent(projectDashboard: ProjectDashboard | null) {
     if (projectDashboard) {
-        return <div/>
+        return (
+            <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+                <div key="a" data-grid={{x: 0, y: 0, w: 1, h: 2, static: true}}>a</div>
+                <div key="b" data-grid={{x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4}}>b</div>
+                <div key="c" data-grid={{x: 4, y: 0, w: 1, h: 2}}>c</div>
+            </GridLayout>
+        )
     } else {
-        return <div/>
+        return (
+            <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+                <div key="a">a</div>
+                <div key="b">b</div>
+                <div key="c">c</div>
+            </GridLayout>
+        )
     }
 }
 
